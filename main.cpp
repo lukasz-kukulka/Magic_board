f5e7e1573e7f4e382e443e3a0f3d4ccf7abb9a60
lukasz-kukulka <62709535+lukasz-kukulka@users.noreply.github.com>
2020-03-29 16:14:59 -0300

    project begin

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My program");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+
+        window.clear();
+
+
+        window.display();
+    }
+
+    return 0;
+}
